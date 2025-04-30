55ac8009b9fa1f008182e39836102b71a9d5fc9b
oltaahasimja <149635414+oltahasimja@users.noreply.github.com>
2025-05-01 01:14:10 +0200

    Fixed Forgot Password issue
    Co-Authored-By: EdinaBajraktari <150193543+EdinaBajraktari@users.noreply.github.com>
    Co-Authored-By: GretaGashi <150357394+GretaGashi@users.noreply.github.com>

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const { Op } = require('sequelize');
 const nodemailer = require('nodemailer');
-const User = require('../infrastructure/database/models/MySQL/User'); 
+const { User }  = require('../infrastructure/database/models/index'); 
 const UserMongo = require('../infrastructure/database/models/Mongo/UserMongo');
 // Configure email transporter
 const transporter = nodemailer.createTransport({
